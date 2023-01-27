653269b2f7981a5837b1e434e34594e31e241cc8
TheLazron <kaushik.rohan31@gmail.com>
2023-01-27 19:02:46 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Text } from "@mantine/core";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Text size="xs" className="text-purple-600">
+        Welcome to Mantine!
+      </Text>
+    </div>
+  );
+}
+
+export default App;
