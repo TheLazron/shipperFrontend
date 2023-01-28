fa453cfc348ae2a025b98ef2a2c26535b6aa8c00
Shivansh Singh <ss398@snu.edu.in>
2023-01-28 18:01:51 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import { useState } from "react";
 import { Text } from "@mantine/core";
 import "./App.css";
 import MainPage from "./mainPage";
-import { TokenProvider } from "./contexts/tokenContext";
 
 
 function App() {
   return (
-    <TokenProvider>
     <div className="App">
       <MainPage />
     </div>
-    </TokenProvider>
   );
 }
 
